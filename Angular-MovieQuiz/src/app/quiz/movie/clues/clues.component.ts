33fca039629ee0d9824f5e3fdda6f83707c275be
gorj00 <os.jangorol@gmail.com>
2019-05-12 01:36:13 +0200

    Setting up fetching images

--- Angular-MovieQuiz/src/app/quiz/movie/clues/clues.component.ts
+++ Angular-MovieQuiz/src/app/quiz/movie/clues/clues.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-clues',
   templateUrl: './clues.component.html',
-  styleUrls: ['./clues.component.css']
+  styleUrls: ['./clues.component.css'],
+  providers: [MovieService]
 })
 export class CluesComponent implements OnInit {
   @Input() singleMovNum: number;
+  cluesImgs: string[] = [];
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
+
+  /**
+   * Gets the movie's 4 image URLs
+   */
+  // trying out Avengers Endgame ID 299534
+  getImages() {
+    this.movieService.getMovImgUrls(299534).subscribe(
+      (response: Response) => {
+
+        // Image objects location: Fetched Data → images → backdrops
+        const imgUrls: string[] = [];
+        const nestedPath: string[] = ['images', 'backdrops', 'file_path'];
+
+        const imgObjects = response[nestedPath[0]][nestedPath[1]].slice(0, 4);
+        imgObjects.map(image => imgUrls.push('http://image.tmdb.org/t/p/w185'
+          + image[nestedPath[2]]));
+        this.cluesImgs = imgUrls;
+        console.log(imgUrls);
+      },
+      error => console.log(error)
+    );
+  }
 
   ngOnInit() {
+    // trying out Avengers Endgame ID 299534
+    this.getImages();
   }
 
 }
---
+++ Angular-MovieQuiz/src/app/services/movie.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class MovieService {
+  baseUrl = 'https://api.themoviedb.org/3/';
+  apiKey = '?api_key=a5357212f9c747dac679fc5ab1aa7ca9';
+
+  constructor(private http: HttpClient) {}
+
+  getMovImgUrls(value: number) {
+      return this.http.get(this.baseUrl + 'movie/' + value + this.apiKey +
+        '&language=en-US' +
+        '&append_to_response=images' +
+        '&include_image_language=en,' +
+        'null');
+  }
+}
