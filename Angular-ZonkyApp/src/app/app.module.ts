b80652d936a67ea29bf7dfbfa8ad3f14f83eba61
gorj00 <os.jangorol@gmail.com>
2019-08-27 21:52:32 +0200

    Components footer, main, sidebar

--- Angular-ZonkyApp/src/app/app.module.ts
+++ Angular-ZonkyApp/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+// Angular Material
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+// ng-bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
