e3169bf79e29849b3dbab66c80c97546cf9458ce
gorj00 <os.jangorol@gmail.com>
2019-08-28 15:19:11 +0200

    Comments removed

--- Angular-ZonkyApp/src/app/services/data-xhr.service.ts
+++ Angular-ZonkyApp/src/app/services/data-xhr.service.ts
@@ -9,14 +9,6 @@ export class DataXHRService {
   constructor(private http: HttpClient) {}
 
   getLoans() {
-    // let loansTotal: number;
-    // const loansPerPage;
-    // const loansPages;
-    // {headersOptions.get('X-Total')}
-    // let headersOptions = new HttpHeaders();
-    // // headersOptions.append('Content-Type', 'application/json');
-    // headersOptions = headersOptions.get('X-Total');
-
     return this.http.get(
       'https://private-anon-2aa5be6b64-zonky.apiary-proxy.com/loans/marketplace',
       { observe: 'response' }
