a776ef5068746fed5dd340e42d9ac9304c258d35
gorj00 <os.jangorol@gmail.com>
2019-09-05 17:01:42 +0200

    USD converter added

--- Angular-ZonkyApp/src/app/services/data-xhr.service.ts
+++ Angular-ZonkyApp/src/app/services/data-xhr.service.ts
@@ -14,13 +14,12 @@ export class DataXHRService {
   constructor(private http: HttpClient) {
     this.getAllLoansCount().subscribe();
     this.getAllLoansAmountTotal().subscribe(response => {
-      console.log(response);
       let totalAmount = 0;
       let icr = 0;
       for (const loansPage of response) {
         icr++;
         for (const loan of loansPage.body) {
-          console.log(icr + ' : ' + loan.amount);
+          // console.log(icr + ' : ' + loan.amount);
           totalAmount += loan.amount;
         }
       }
@@ -57,9 +56,18 @@ export class DataXHRService {
 
   getAllLoansAmountTotal() {
     const observablesJoin: Observable<any>[] = [];
-    for (let i = 1; i <= this.getTotalForLoop(45, 20); i++) {
-      observablesJoin.push(this.getLoansPage(i));
-    }
+
+    for (let i = 1; i <= this.getTotalForLoop(30000, 20); i++) {
+      // Delay 200s every 10 requests for browsers not to crush
+      if (i % 10 === 0) {
+        setTimeout(() => {
+          observablesJoin.push(this.getLoansPage(i));
+        }, 500);
+      } else {
+        observablesJoin.push(this.getLoansPage(i));
+      } // end else
+    } // end for
+
     return forkJoin([...observablesJoin]);
   }
 
